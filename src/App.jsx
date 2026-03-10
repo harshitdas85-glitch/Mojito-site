e1066db22f74a44d0cf0abc6aec43dd792f28853
HarshitDas <harshitdas85@gmail.com>
2026-03-11 00:30:41 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Gsapto from './Pages/gsapto'
+
+
 import About from './components/About.jsx'
-import Gsapfrom from './Pages/Gsapfrom'
-import GsapFromto from './Pages/GsapFromto'
+
 import gsap from 'gsap'
 import Contact from './components/Contact.jsx'
 import Cocktails from "./components/Cocktails.jsx"
-import Gsaptimeline from './Pages/Gsaptimeline'
+
 import {ScrollTrigger,SplitText} from 'gsap/all'
 import Menu from './components/Menu.jsx'
 gsap.registerPlugin(ScrollTrigger,SplitText)
