b46e8370c21bb8804d3bdd3abdc53acd8924518f
HarshitDas <harshitdas85@gmail.com>
2026-03-11 00:18:56 +0530

    edit pages

--- src/Pages/GsapFromto.jsx
+++ src/Pages/GsapFromto.jsx
@@ -1,30 +1,43 @@
 import React from 'react'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-function GsapFromto() {
 
-      useGSAP(()=>{
-    gsap.fromTo('#yellow',{
-        x:0,
-        rotation:0,
-        borderRadius:'0%'
-    },{
-        x:250,
-        repeat:-1,
-        yoyo:true,
-        borderRadius:'60%',
-        rotation:360,
-        duration:3,
-        ease:'bounce-in'
-        
-    })
-    },[])
+const GsapFromto = () => {
   return (
-
-  <div id='yellow' className='w-20 mt-20 h-20 bg-blue-500 rounded-lg'>
+    <div>
       
     </div>
   )
 }
 
 export default GsapFromto
+
+
+// import React from 'react'
+// import gsap from 'gsap'
+// import { useGSAP } from '@gsap/react'
+// function GsapFromto() {
+
+//       useGSAP(()=>{
+//     gsap.fromTo('#yellow',{
+//         x:0,
+//         rotation:0,
+//         borderRadius:'0%'
+//     },{
+//         x:250,
+//         repeat:-1,
+//         yoyo:true,
+//         borderRadius:'60%',
+//         rotation:360,
+//         duration:3,
+//         ease:'bounce-in'
+        
+//     })
+//     },[])
+//   return (
+
+//   <div id='yellow' className='w-20 mt-20 h-20 bg-blue-500 rounded-lg'>
+      
+//     </div>
+//   )
+// }
+
+// export default GsapFromto
--- src/Pages/Gsapfrom.jsx
+++ src/Pages/Gsapfrom.jsx
@@ -1,23 +1,45 @@
 import React from 'react'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-function Gsapfrom() {
-      useGSAP(()=>{
-    gsap.from('#blue-box',{
-        x:250,
-        repeat:-1,
-        yoyo:true,
-        rotation:360,
-        duration:3,
-        ease:'bounce.in'
-        
-    })
-    },[])
+
+const Gsapfrom = () => {
   return (
-      <div id='blue-box' className='w-20 h-20 bg-green-500 rounded-lg'>
+    <div>
       
     </div>
   )
 }
 
 export default Gsapfrom
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import gsap from 'gsap'
+// import { useGSAP } from '@gsap/react'
+// function Gsapfrom() {
+//       useGSAP(()=>{
+//     gsap.from('#blue-box',{
+//         x:250,
+//         repeat:-1,
+//         yoyo:true,
+//         rotation:360,
+//         duration:3,
+//         ease:'bounce.in'
+        
+//     })
+//     },[])
+//   return (
+//       <div id='blue-box' className='w-20 h-20 bg-green-500 rounded-lg'>
+      
+//     </div>
+//   )
+// }
+
+// export default Gsapfrom
